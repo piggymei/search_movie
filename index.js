1f8c31f66d33be241516b68d664d12248ab74942
Jane <yumeij.2010@gmail.com>
2020-07-20 15:24:28 -0700

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.get("/movies", (req, res) => {
   const url = `http://omdbapi.com/?apikey=${process.env.APIKEY}&s=${req.query.s}`;
 
-  axios.get(url).then((movies) => console.log(movies.data));
+  axios.get(url).then((movies) => res.send(movies.data));
 });
 
 app.listen(process.env.PORT || 3000, () => {
