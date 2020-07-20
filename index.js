ccf80540bf14480f899b54df1c70d1e581fdd1d3
Jane <yumeij.2010@gmail.com>
2020-07-20 14:54:44 -0700

    updates

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+
+app.get("/movies", (req, res) => {
+  const url = `http://omdbapi.com/?apikey={APIKEY}&s=${req.query}`;
+
+  axios
+    .get(url)
+    .then((response) => response.json())
+    .then((movies) => console.log(movies.data));
+});
+
+app.listen(process.env.Port || 3000, () => {
+  console.log("listening");
+});
